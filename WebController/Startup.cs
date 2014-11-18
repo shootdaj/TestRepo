55b49916ce374a67b32b5636aebb0b0c581b4388
PLASMA <shootdaj@gmail.com>
2014-11-17 23:29:48 -0500

    Replaced old MVC project with new MVC5 project.

---
+++ WebController/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebController.Startup))]
+namespace WebController
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
