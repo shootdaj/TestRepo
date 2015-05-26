248d0555f213b30f3419a6366f2c4f5a709315dc
NOVA <shootdaj@gmail.com>
2015-05-26 15:49:56 -0400

    Added Console/app.config to the list of files to be moved. This should be eventually taken out and the Console project should be removed from the "test" solution. Maybe make a new solution that just contains test projects and projects required by test projects.

--- PullDownConfig.bat
+++ PullDownConfig.bat
@@ -1,3 +1,4 @@
 git clone https://github.com/shootdaj/ZoneLightingTestConfig temp
 mv temp/Tests/ZoneLightingTests/app.config Tests/ZoneLightingTests/app.config
+mv temp/Console/app.config Console/app.config
 rm -rf temp
